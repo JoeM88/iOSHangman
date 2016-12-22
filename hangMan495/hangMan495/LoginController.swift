3cf7a7b4b6a748ec66bdc909fcfc98949e47905c
Joseph Molina <jomolina@csumb.edu>
2016-12-21 17:08:49 -0800

    added login screen

---
+++ hangMan495/hangMan495/LoginController.swift
@@ -0,0 +1,13 @@
+//
+//  LoginController.swift
+//  hangMan495
+//
+//  Created by Joseph Molina on 12/21/16.
+//  Copyright © 2016 Joseph Molina. All rights reserved.
+//
+
+import Foundation
+class LoginController: UIViewController {
+    
+
+}
