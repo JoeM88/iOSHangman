f09a0b248e066ac61412dcb5421914efa0dd9677
Joseph Molina <jomolina@csumb.edu>
2016-12-24 00:03:16 -0800

    Finished the logout functionality

--- hangMan495/hangMan495/ViewController.swift
+++ hangMan495/hangMan495/ViewController.swift
@@ -7,20 +7,24 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func logoutAction(_ sender: Any) {
+        if FIRAuth.auth()?.currentUser != nil {
+            do {
+                try FIRAuth.auth()?.signOut()
+                 let vc = self.storyboard?.instantiateViewController(withIdentifier: "LoginScreen")
+                present(vc!, animated: true, completion: nil)
+                
+            } catch let error as NSError {
+                print(error.localizedDescription)
+            }
+        }
     }
     
-
-
 }
 
