89b133b8c40e94eec7102bace375284caa4d14e3
Joseph Molina <jomolina@csumb.edu>
2016-12-23 22:55:02 -0800

    Finished register functionality

--- hangMan495/hangMan495/RegisterViewController.swift
+++ hangMan495/hangMan495/RegisterViewController.swift
@@ -7,29 +7,45 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
+
 
 class RegisterViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    //Outlets  
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    //Sign Up Action for email
     
+    @IBAction func createAccountAction(_ sender: Any) {
+            if emailTextField.text == "" {
+                let alertController = UIAlertController(title: "Error", message: "Please enter your email and password", preferredStyle: .alert)
+                
+                let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                alertController.addAction(defaultAction)
+                
+                present(alertController, animated: true, completion: nil)
+                
+            } else {
+                FIRAuth.auth()?.createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
+                    
+                    if error == nil {
+                        print("You have successfully signed up")
+                        
+                        let vc = self.storyboard?.instantiateViewController(withIdentifier: "MainScreen")
+                        self.present(vc!, animated: true, completion: nil)
+                        
+                    } else {
+                        let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                        
+                        let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                        alertController.addAction(defaultAction)
+                        
+                        self.present(alertController, animated: true, completion: nil)
+                    }
+                }
+            }
+        }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
     }
-    */
-
-}
